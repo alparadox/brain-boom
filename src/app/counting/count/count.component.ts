f618a4ca4391229870c1f350bc52fbfcd73eb0fd
Alyona <alyona.dux@yandex.ru>
2022-11-22 14:50:43 +0300

    добавлена логика показа ответа на предыдущий пример

--- src/app/counting/count/count.component.ts
+++ src/app/counting/count/count.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ExerciseHistoryService} from "../exercise-history.service";
 
 @Component({
   selector: 'app-count',
@@ -9,11 +8,11 @@ import {ExerciseHistoryService} from "../exercise-history.service";
 export class CountComponent implements OnInit {
 
   public exercise: string = '';
+  public previousAnswer: string = '';
   private numberOne: number = 0;
   private numberTwo: number = 0;
 
   constructor(
-    private exerciseHistoryService: ExerciseHistoryService
   ) {}
 
 
@@ -21,9 +20,18 @@ export class CountComponent implements OnInit {
     this.getMathExercise();
   }
 
-  public getMathExercise(): string {
+  public goNextExercise(): void {
+    this.getAnswerPreviousExercise();
+    this.getMathExercise();
+  }
+
+  private getMathExercise(): string {
     this.numberOne = Math.round(Math.random() * 10);
     this.numberTwo = Math.round(Math.random() * 10);
     return this.exercise = `${this.numberOne} + ${this.numberTwo}`
+  }
+
+  private getAnswerPreviousExercise(): string {
+    return this.previousAnswer = `${this.exercise} = ${this.numberOne + this.numberTwo}`;
   }
 }
