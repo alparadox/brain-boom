f95c5895412e99a8063d469d678f21794ce8fea8
Alyona <alyona.dux@yandex.ru>
2023-02-06 23:19:25 +0300

    build

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/brain-boom'),
+      dir: require('path').join(__dirname, './coverage/docs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
